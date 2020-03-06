d49348fc74740f30de10456e69580cc006e3d7a1
maximog <maximo.gijon@gmail.com>
2020-03-06 16:30:06 -0300

    separating ecomm app into client folder, added express server to help handle backend stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = "pk_test_qjMQbZteXhcicdzjZBYX1MOm00o21iFJaA";
 
     const onToken = token => {
-        console.log(token);
-        alert("Payment Succesful!");
-    }
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        })
+            .then(response => {
+                alert('Payment successful');
+            })
+            .catch(error => {
+                console.log('Payment error', JSON.parse(error));
+                alert(
+                    'There was an issue with your payment. Please make sure you use the provided credit card'
+                );
+            });
+    };
 
     return (
-        <StripeCheckout 
+        <StripeCheckout
             label='Pay Now'
             name='ECOMMERCE APP'
             billingAddress
