fe299a3014b7a73538de0776857304e60b905c38
maximog <maximo.gijon@gmail.com>
2020-04-15 21:17:09 -0300

    updated mobile responsiveness for collection-item and menu-item components

--- client/src/components/menu-item/menu-item.styles.jsx
+++ client/src/components/menu-item/menu-item.styles.jsx
@@ -15,12 +15,12 @@ export const MenuItemContainer = styled.div`
     &:hover {
         cursor: pointer;
         & .background-image {
-			transform: scale(1.1);
-			transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
-		}
-		& .content {
-			opacity: 0.9;
-		}
+	transform: scale(1.1);
+	transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+	}
+	& .content {
+	  opacity: 0.9;
+	}
       }
 
       &:first-child {
